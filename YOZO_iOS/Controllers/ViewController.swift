ce473307d729745e00ed58736bb3e92d137d93bc
hsseor <hsseor@naver.com>
2023-04-12 10:13:01 +0900

    [Add] initial setting, extensions, asset

---
+++ YOZO_iOS/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YOZO_iOS
+//
+//  Created by 홍서린 on 2023/04/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
