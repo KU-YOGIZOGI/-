5831f9c042bd9fff25411fb1dc3957071e2b0b4f
Seorin Hong <68605075+hsseor@users.noreply.github.com>
2023-06-09 15:26:13 +0900

    Delete YOZO_iOS directory

--- YOZO_iOS/Controllers/MapInfo.swift
+++
@@ -1,8 +0,0 @@
-//
-//  MapInfo.swift
-//  YOZO_iOS
-//
-//  Created by 홍서린 on 2023/04/25.
-//
-
-import Foundation
--- YOZO_iOS/Services/reviewAPI.swift
+++
@@ -1,8 +0,0 @@
-//
-//  reviewAPI.swift
-//  YOZO_iOS
-//
-//  Created by 홍서린 on 2023/05/20.
-//
-
-import Foundation
